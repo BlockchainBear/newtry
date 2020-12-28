26fc8975fbaaf2c74d8da4b3015cd1a5fea69fc3
BlockchainBear <64762751+BlockchainBear@users.noreply.github.com>
2020-12-27 16:31:03 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import TodoList from './TodoList'
 
 function App() {
-  const [todos, setTodos] = useState(["todo 1", "Todo 2"])
+  const [todos, setTodos] = useState([{ id:1 , name: "todo 1", complete: false}])
   return (
    < > 
    <TodoList todos={todos}/>
