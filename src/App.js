b912e94eccfec852a4038516dc783fd5e97b5d5d
BlockchainBear <64762751+BlockchainBear@users.noreply.github.com>
2020-12-27 19:20:50 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './TodoList'
 //import uuidv4 from 'uuid/v4'
 
+const localStorageKey = 'todoApp.todos'
 function App() {
   const [todos, setTodos] = useState([])
   const todoNameRef = useRef() 
+
+  useEffect(()=> {
+const storedTodos = JSON.parse(localStorage.getItem(localStorageKey))
+if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() =>{
+    localStorage.setItem(localStorageKey, JSON.stringify(todos))
+  },[todos])
+
+function toggleTodo(id){
+  const newTodos = [...todos]
+  const todo = newTodos.find(todo => todo.id === id)
+  todo.complete = !todo.complete
+  setTodos(newTodos)
+}
+
   function handleAddTodo(e){
     const name = todoNameRef.current.value 
     if (name === '') return
@@ -15,7 +33,7 @@ function App() {
   }
   return (
    < > 
-   <TodoList todos={todos}/>
+   <TodoList todos={todos} toggleTodo ={toggleTodo}/>
     <input ref={todoNameRef}type="text"></input>
     <button onClick={handleAddTodo}>add todo</button>
     <button>Clear Complete</button>
